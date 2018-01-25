9c4b598739a687ab6951dff08bc3f2dccfd2d932
Weerapat <weerapatsu@gmail.com>
2018-01-25 13:43:41 +0700

    Optimize code

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -12,29 +12,140 @@ let agentMachines = [
         'name': 'PC 3',
         'guacid': 9,
         'guacid_thumb': 7
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
+    },
+    {
+        'name': 'PC 2',
+        'guacid': 8,
+        'guacid_thumb': 5
     }
 ];
 
-// Get display div from document
-let display = document.getElementById("display");
-
-let guac = new Guacamole.Client(
-    new Guacamole.WebSocketTunnel('ws://guacamole.rabbit/guacamole/websocket-tunnel')
-);
-
-let guac2 = new Guacamole.Client(
-    new Guacamole.WebSocketTunnel('ws://guacamole.rabbit/guacamole/websocket-tunnel')
-);
-
+// Fulscreen set
 let guacFull = new Guacamole.Client(
     new Guacamole.WebSocketTunnel('ws://guacamole.rabbit/guacamole/websocket-tunnel')
 );
 
-// Add client to display div
-display.appendChild(guac.getDisplay().getElement());
 
-document.getElementById('display-2').appendChild(guac2.getDisplay().getElement());
-document.getElementById('display-fullscreen').appendChild(guacFull.getDisplay().getElement());
+document.getElementById('display-monitor').appendChild(guacFull.getDisplay().getElement());
 
 // Mouse
 let mouse = new Guacamole.Mouse(guacFull.getDisplay().getElement());
@@ -48,20 +159,17 @@ mouse.onmousedown =
 // Keyboard
 let keyboard = new Guacamole.Keyboard(document);
 
-keyboard.onkeydown = function (keysym) {
+keyboard.onkeydown = function(keysym) {
     guacFull.sendKeyEvent(1, keysym);
 };
 
-keyboard.onkeyup = function (keysym) {
+keyboard.onkeyup = function(keysym) {
     guacFull.sendKeyEvent(0, keysym);
 };
+
+// End Fullscreenset
+
 // End mouse
-
-// Error handler
-guac.onerror = function(error) {
-    alert(error);
-};
-
 let authToken = null;
 
 $.post('http://guacamole.rabbit/guacamole/api/tokens',
@@ -70,11 +178,31 @@ $.post('http://guacamole.rabbit/guacamole/api/tokens',
         password: 'guacadmin'
     }, (data) => {
         authToken = data.authToken;
-        guac.connect(`GUAC_DATA_SOURCE=mysql&GUAC_TYPE=c&GUAC_ID=7&token=${authToken}`);
-        guac2.connect(`GUAC_DATA_SOURCE=mysql&GUAC_TYPE=c&GUAC_ID=5&token=${authToken}`);
-        guacFull.connect(`GUAC_DATA_SOURCE=mysql&GUAC_TYPE=c&GUAC_ID=8&token=${authToken}`);
+
+        let guac = [];
+
+        $.each(agentMachines, function(i, agent) {
+            guac[i] = new Guacamole.Client(
+                new Guacamole.WebSocketTunnel('ws://guacamole.rabbit/guacamole/websocket-tunnel')
+            );
+
+            guac[i].connect(`GUAC_DATA_SOURCE=mysql&GUAC_TYPE=c&GUAC_ID=${agent.guacid_thumb}&token=${authToken}`);
+
+            // Add client to display div
+            $('.thumbnails').append(guac[i].getDisplay().getElement());
+
+            // Error handler
+            guac[i].onerror = function(error) {
+                console.log(error);
+            };
+        });
+
+
+        // temporary connection
+        guacFull.connect(`GUAC_DATA_SOURCE=mysql&GUAC_TYPE=c&GUAC_ID=8&GUAC_WIDTH=250&GUAC_HEIGHT=350&GUAC_DPI=192&GUAC_AUDIO=audio/L8&token=${authToken}`);
     }
 );
+
 
 $('.monitor-thumbnails').on('click', function () {
     let guactId = $(this).data('id');
@@ -84,7 +212,22 @@ $('.monitor-thumbnails').on('click', function () {
 function switchView(guacId) {
     guacFull.disconnect();
     guacFull.connect(`GUAC_DATA_SOURCE=mysql&GUAC_TYPE=c&GUAC_ID=${guacId}&GUAC_WIDTH=250&GUAC_HEIGHT=350&GUAC_DPI=192&GUAC_AUDIO=audio/L8&token=${authToken}`);
+
+    //$('#display-monitor').fadeIn();
 }
+
+/**
+ *    STATE_IDLE          = 0;
+ *    STATE_CONNECTING    = 1;
+ *    STATE_WAITING       = 2;
+ *    STATE_CONNECTED     = 3;
+ *    STATE_DISCONNECTING = 4;
+ *    STATE_DISCONNECTED  = 5;
+ * @param stateNumber
+ */
+guacFull.onstatechange = function (stateNumber) {
+    console.log(stateNumber);
+};
 
 // Disconnect on close
 window.onunload = function() {
